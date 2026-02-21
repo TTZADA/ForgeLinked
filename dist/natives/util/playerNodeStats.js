36b59589cbd9c92046d8c099f74e46eb578a2a70
Sun.it <114196209+TTZADA@users.noreply.github.com>
2026-02-21 13:44:38 -0300

    Update playerNodeStats.js

--- dist/natives/util/playerNodeStats.js
+++ dist/natives/util/playerNodeStats.js
@@ -24,19 +24,15 @@ exports.default = new forgescript_1.NativeFunction({
             return this.customError('ForgeLinked is not initialized');
         let node;
         if (nodeId) {
-            // Try to get a specific node by ID
-            // @ts-ignore - nodes is expected to be a Map-like structure
-            node = linked.nodeManager.nodes?.get?.(String(nodeId));
+            node = linked.nodeManager.nodes.get(String(nodeId));
         }
         else {
-            // Fallback to the first available node
-            // @ts-ignore - nodes is expected to be an iterable of node values
-            const values = linked.nodeManager.nodes?.values?.();
-            node = values ? values.next().value : undefined;
+            const values = linked.nodeManager.nodes.values();
+            node = values.next().value;
         }
         if (!node)
             return this.customError('Lavalink node not found');
-        return this.successJSON(node.stats ?? {});
+        return this.successJSON({ name: node.id, ...(node.stats ?? {}) });
     },
 });
 //# sourceMappingURL=playerNodeStats.js.map
