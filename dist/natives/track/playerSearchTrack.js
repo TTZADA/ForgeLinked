5e68f31c9d345e4cb8ccf71ebe3f49425def17ac
Zerott <114196209+imzerott@users.noreply.github.com>
2026-06-06 06:32:57 -0300

    xd

--- dist/natives/track/playerSearchTrack.js
+++ dist/natives/track/playerSearchTrack.js
@@ -57,7 +57,7 @@ exports.default = new forgescript_1.NativeFunction({
         const supported = info.sourceManagers || [];
         let finalQuery = query;
         if (source) {
-            if (!source.includes(supported)) {
+            if (!supported.includes(source)) {
                 return this.customError(`Source '${source}' not supported by the Lavalink server`);
             }
             finalQuery = `${source}:${query}`;
