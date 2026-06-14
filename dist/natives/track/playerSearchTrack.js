6469b30eebaac1bebac59b60eab681e0d4ceac26
Zerott <114196209+imzerott@users.noreply.github.com>
2026-06-14 10:58:09 -0300

    .

--- dist/natives/track/playerSearchTrack.js
+++ dist/natives/track/playerSearchTrack.js
@@ -57,7 +57,7 @@ exports.default = new forgescript_1.NativeFunction({
         const supported = info.sourceManagers || [];
         let finalQuery = query;
         if (source) {
-            if (!supported.includes(source) || source !== 'youtubemusic') {
+            if (!supported.includes(source) && source !== 'youtubemusic') {
                 return this.customError(`Source '${source}' not supported by the Lavalink server`);
             }
             finalQuery = `${source.replace('youtubemusic', 'ytmsearch')}:${query}`;
