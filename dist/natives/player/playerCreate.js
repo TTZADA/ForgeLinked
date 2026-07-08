bb6deaf585ccbd39054142aba9094133d59042b0
Zerott <114196209+imzerott@users.noreply.github.com>
2026-07-08 17:27:15 -0300

    .

--- dist/natives/player/playerCreate.js
+++ dist/natives/player/playerCreate.js
@@ -78,11 +78,11 @@ async execute(ctx, [guildId, voiceId, textId, volume, selfDeaf, selfMute, node])
     const player = linked.players.get(guildId.id);
     if (!player)
         return this.success(false);
-    
-    await player.disconnect(true);
-    await new Promise(r => setTimeout(r, 300));
-    await player.connect();
 
+if (node.includes('Premium')) {
+    await player.changeNode(node)
+}
+        
     return this.success(linked.players.has(guildId.id));
 },
 });
