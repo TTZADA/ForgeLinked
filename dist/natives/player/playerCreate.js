82b83cdfe075ef60f937a3f30ed643f8decae6ce
Zerott <114196209+imzerott@users.noreply.github.com>
2026-07-08 17:45:31 -0300

    .

--- dist/natives/player/playerCreate.js
+++ dist/natives/player/playerCreate.js
@@ -60,26 +60,20 @@ exports.default = new forgescript_1.NativeFunction({
         },
     ],
     output: forgescript_1.ArgType.Boolean,
-async execute(ctx, [guildId, voiceId, textId, volume, selfDeaf, selfMute, node]) {
-    const linked = ctx.client.getExtension(index_js_1.ForgeLinked, true).lavalink;
-    if (!linked)
-        return this.customError('ForgeLinked is not initialized');
-
-    linked.createPlayer({
-        guildId: guildId.id,
-        voiceChannelId: voiceId.id,
-        textChannelId: textId?.id || ctx.channel?.id,
-        volume: volume || 100,
-        selfDeaf: selfDeaf ?? true,
-        selfMute: selfMute || false,
-        node: node || undefined,
-    });
-
-    const player = linked.players.get(guildId.id);
-    if (!player)
-        return this.success(false);
-        
-    return this.success(linked.players.has(guildId.id));
-},
+    async execute(ctx, [guildId, voiceId, textId, volume, selfDeaf, selfMute, node]) {
+        const linked = ctx.client.getExtension(index_js_1.ForgeLinked, true).lavalink;
+        if (!linked)
+            return this.customError('ForgeLinked is not initialized');
+        linked.createPlayer({
+            guildId: guildId.id,
+            voiceChannelId: voiceId.id,
+            textChannelId: textId?.id || ctx.channel?.id,
+            volume: volume || 100,
+            selfDeaf: selfDeaf || true,
+            selfMute: selfMute || false,
+            node: node || undefined,
+        });
+        return this.success(linked.players.has(guildId.id));
+    },
 });
 //# sourceMappingURL=playerCreate.js.map
