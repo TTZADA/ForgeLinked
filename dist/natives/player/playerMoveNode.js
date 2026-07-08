67ba5d2d322bd3d65307c09d4f68698e904ff215
Zerott <114196209+imzerott@users.noreply.github.com>
2026-07-08 14:36:24 -0300

    .

--- dist/natives/player/playerMoveNode.js
+++ dist/natives/player/playerMoveNode.js
@@ -73,6 +73,11 @@ exports.default = new forgescript_1.NativeFunction({
         }
 
         try {
+            if (typeof player.changeNode === 'function') {
+                await player.changeNode(targetNode);
+                return this.success(true);
+            }
+
             const currentTrack = player.queue.current;
             const lastPosition = player.position || 0;
             const wasPaused = player.paused;
