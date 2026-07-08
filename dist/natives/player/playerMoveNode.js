e26ff253814b5dbe96a80ca049fe3e7c24ce1361
Zerott <114196209+imzerott@users.noreply.github.com>
2026-07-08 11:24:01 -0300

    .

---
+++ dist/natives/player/playerMoveNode.js
@@ -0,0 +1,74 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const forgescript_1 = require("@tryforge/forgescript");
+const index_js_1 = require("../../index.js");
+exports.default = new forgescript_1.NativeFunction({
+    name: '$playerMoveNode',
+    description: 'Move a player to a different lavalink node',
+    version: '1.0.0',
+    brackets: true,
+    unwrap: true,
+    args: [
+        {
+            name: 'guildId',
+            description: 'The guild id of the player to move',
+            type: forgescript_1.ArgType.Guild,
+            required: true,
+            rest: false,
+        },
+        {
+            name: 'nodeId',
+            description: 'The target node id to move the player to',
+            type: forgescript_1.ArgType.String,
+            required: false,
+            rest: false,
+        },
+        {
+            name: 'delay',
+            description: 'Delay in milliseconds before moving the node',
+            type: forgescript_1.ArgType.Number,
+            required: false,
+            rest: false,
+        },
+        {
+            name: 'pauseBefore',
+            description: 'Whether to pause the player before moving',
+            type: forgescript_1.ArgType.Boolean,
+            required: false,
+            rest: false,
+        },
+    ],
+    output: forgescript_1.ArgType.Boolean,
+    async execute(ctx, [guildId, nodeId, delay, pauseBefore]) {
+        const linked = ctx.client.getExtension(index_js_1.ForgeLinked, true).lavalink;
+        if (!linked)
+            return this.customError('ForgeLinked is not initialized');
+
+        const player = linked.getPlayer(guildId.id);
+        if (!player)
+            return this.customError('No player found for this guild');
+
+        const targetNode = nodeId
+            ? linked.nodeManager.nodes.get(nodeId)
+            : linked.nodeManager.leastUsedNodes[0];
+
+        if (!targetNode)
+            return this.customError('Target node not found or unavailable');
+
+        if (targetNode.id === player.node?.id)
+            return this.success(false);
+
+        if (delay && delay > 0) {
+            await new Promise((res) => setTimeout(res, delay));
+        }
+
+        if (pauseBefore && !player.paused && player.queue.current) {
+            await player.pause(true);
+        }
+
+        await player.moveNode(targetNode);
+
+        return this.success(true);
+    },
+});
+//# sourceMappingURL=playerMoveNode.js.map
